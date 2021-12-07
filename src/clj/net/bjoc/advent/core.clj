e4b10e98168e7f2c929727b2e85af448255842ae
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2021-12-07 14:45:19 -0600

    Add defrunner macro for more concise puzzle code

--- src/clj/net/bjoc/advent/core.clj
+++ src/clj/net/bjoc/advent/core.clj
@@ -24,3 +24,16 @@
      (let [m (-> ns-symbol find-ns ns-map)]
        ((m 'part-1))
        ((m 'part-2))))))
+
+(defmacro defrunner
+  [part-number fn-name description & additional-args]
+  (let [[_ year day] (re-find #"year-(\d+)\.day-(\d+)$" (str (ns-name *ns*)))
+        filename (format "data/year_%s/day_%s.input" year day)
+        desc (format "%s - Day %s, Part %s - %s:"
+                     year day part-number description)
+        part-sym (symbol (format "part-%s" part-number))]
+    (list 'defn part-sym '[]
+          (list 'println desc
+                (-> additional-args
+                    (conj filename)
+                    (conj fn-name))))))

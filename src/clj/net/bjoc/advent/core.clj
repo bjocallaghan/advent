5de1136827f261d8c18d9e8f79d2fdda83f7cbe6
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2021-12-08 10:47:21 -0600

    Cleanup core.clj (minor)

--- src/clj/net/bjoc/advent/core.clj
+++ src/clj/net/bjoc/advent/core.clj
@@ -1,12 +1,7 @@
 (ns net.bjoc.advent.core
-  (:use [clojure.java.shell :only [sh]])
   (:require [clojure.string :as str]
             [taoensso.timbre :as timbre
-             ;; :refer [log  trace  debug  info  warn  error  fatal  report
-             ;;         logf tracef debugf infof warnf errorf fatalf reportf
-             ;;         spy get-env]])
-             :refer [trace debug info warn error fatal]]
-            ))
+             :refer [trace debug info warn error fatal]]))
 
 (def this-ns-stem (->> *ns*
                        ns-name
@@ -17,7 +12,7 @@
 (defn run
   "Entry point to Advent of Code runner."
   [{:keys [year day] :as opts}]
-  (debug "Options:" opts)
+  (trace "Options:" opts)
   (let [ns-symbol (symbol (format "%s.year-%s.day-%s" this-ns-stem year day))]
     (require ns-symbol)
     (time

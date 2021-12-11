057768006f09f387c00d9f6afd225f10cc03e324
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2021-12-11 01:17:38 -0600

    Refactor 2021 day 11

--- src/clj/net/bjoc/advent/year_2021/day_11.clj
+++ src/clj/net/bjoc/advent/year_2021/day_11.clj
@@ -1,8 +1,7 @@
 (ns net.bjoc.advent.year-2021.day-11
   (:use [net.bjoc.advent.util.numeric :only [parse-int]]
         [net.bjoc.advent.util.coords :only [add-coords]])
-  (:require [clojure.string :as str]
-            [clojure.set :as set]
+  (:require [clojure.set :as set]
             [net.bjoc.advent.core :as advent]
             [net.bjoc.advent.util.matrix :as mtx]))
 
@@ -17,40 +16,35 @@
     :or {x-lower 0
          y-lower 0}
     :as bounds} xy]
-  (let [in-bounds? (fn [[x y]] (and (>= x x-lower)
-                                    (>= y y-lower)
-                                    (<= x x-upper)
-                                    (<= y y-upper)))]
+  (let [in-bounds? (fn [[x y]] (and (<= x-lower x x-upper) (<= y-lower y y-upper)))]
     (->> [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]
          (map (partial add-coords xy))
          (filter in-bounds?))))
+
+(defn flashing? [[xy v]] (>= v 10))
 
 (defn step
   ([world] (step world 1))
   ([{:keys [octopuses flashes] :or {flashes 0} :as world} num-steps]
    (if (zero? num-steps)
      world
-     (recur (let [[x-upper y-upper] (mtx/size octopuses)
-                  neighbors* (partial neighbors {:x-upper (dec x-upper)
-                                                 :y-upper (dec y-upper)})
-                  flash (fn [m xy]
-                          (reduce (fn [m xy] (update m xy inc)) m (neighbors* xy)))
-                  unflash (fn [m] (into {} (map (fn [[k v]] [k (if (>= v 10) 0 v)]) m)))
-                  flashing? (fn [[xy v]] (>= v 10))]
-              (loop [m (reduce (fn [m [k v]] (assoc m k (inc v))) {} octopuses)
-                     flashing #{}]
-                (let [new-flashing (set/difference (->> m 
-                                                        (filter flashing?) 
-                                                        keys 
-                                                        (into #{}))
-                                                   flashing)]
-                  (if (empty? new-flashing)
-                    {:octopuses (unflash m)
-                     :flashes (+ flashes (count flashing))
-                     :step-flashes (count flashing)}
-                    (recur (reduce flash m new-flashing)
-                           (set/union flashing new-flashing))))))
-            (dec num-steps)))))
+     (recur
+      (let [[x-lim y-lim] (map dec (mtx/size octopuses))
+            neighbors* (partial neighbors {:x-upper x-lim :y-upper y-lim})
+            flash #(reduce (fn [m xy] (update m xy inc)) %1 (neighbors* %2))
+            unflash #(into {} (map (fn [[k v]] [k (if (>= v 10) 0 v)]) %))]
+        (loop [m (reduce (fn [m [k v]] (assoc m k (inc v))) {} octopuses)
+               flashing #{}]
+          (let [new-flashing (set/difference
+                              (->> m (filter flashing?) keys (into #{}))
+                              flashing)]
+            (if (empty? new-flashing)
+              {:octopuses (unflash m)
+               :flashes (+ flashes (count flashing))
+               :step-flashes (count flashing)}
+              (recur (reduce flash m new-flashing)
+                     (set/union flashing new-flashing))))))
+      (dec num-steps)))))
 
 (defn file->post-steps-flash-count
   ([filename] (file->post-steps-flash-count filename 100))

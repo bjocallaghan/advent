ea6f831f32c77c7d89332f824ff5a97db3b4c2c4
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2021-12-14 17:47:18 -0600

    Add 2021 day 14

---
+++ src/clj/net/bjoc/advent/year_2021/day_14.clj
@@ -0,0 +1,135 @@
+(ns net.bjoc.advent.year-2021.day-14
+  (:use [net.bjoc.advent.util.misc :only [zip]])
+  (:require [clojure.string :as str]
+            [net.bjoc.advent.core :as advent]))
+  
+(defn file->chain [filename]
+  (-> filename
+      slurp
+      (str/split #"\n")
+      first))
+
+(defn file->rules [filename]
+  (let [lines (-> filename
+                  slurp
+                  (str/split #"\n\n")
+                  second
+                  (str/split #"\n"))]
+    (->> lines
+         (map (fn [line]
+                (let [[_ pair insert] (re-find #"^(..) -> (.)$" line)]
+                  [(-> pair seq vec) (first insert)])))
+         (into {}))))
+
+(defn grow-fn [rules]
+  (fn grow
+    ([chain] (grow chain 1))
+    ([chain num-times]
+     (if (zero? num-times)
+       chain
+       (recur (->> (zip chain (drop 1 chain))
+                   (map rules)
+                   (interleave chain)
+                   (#(concat % [(last chain)]))
+                   (remove nil?)
+                   (apply str))
+              (dec num-times))))))
+
+(defn freqs-after-growth
+  "Returns the frequencies of elements in a chain after a number of reactions."
+  [chain rules num-times]
+  (frequencies ((grow-fn rules) chain num-times)))
+
+(defn file->big-small-diff [filename]
+  (let [rules (file->rules filename)
+        chain (file->chain filename)
+        freqs (freqs-after-growth chain rules 10)]
+    (- (-> freqs vals sort last) (-> freqs vals sort first))))
+
+;;;
+
+(defn inc* [x] (inc (or x 0)))
+
+(defn freqs-after-growth*
+  "Returns the frequencies of elements in a chain after a number of reactions.
+
+  I had fun developing this algorithm, and while doing so I /thought/ it would
+  result in a performance boost, but even though it doesn't attempt to do the
+  whole thing at once, the 'chain' stack just keeps growing. So ultimately, this
+  function isn't much more useful than the first."
+  [chain rules num-times]
+  (loop [curr (first chain)
+         chain (rest (seq chain))
+         freqs {curr 1}
+         num-times num-times
+         num-times-stack (repeat (count chain) num-times)]
+    (if (empty? chain)
+      freqs
+      (if-let [insert (and (> num-times 0)
+                           (rules (list curr (first chain))))]
+        (recur curr
+               (conj chain insert)
+               freqs
+               (dec num-times)
+               (conj num-times-stack (dec num-times)))
+        (recur (first chain)
+               (rest chain)
+               (update freqs (first chain) inc*)
+               (first num-times-stack)
+               (rest num-times-stack))))))
+
+;;;
+
+(defn all-keys [rules]
+  (->> rules
+       keys
+       (apply concat)
+       (concat (vals rules))
+       set))
+
+(defn add-freqs* [all-keys & ms]
+  (reduce (fn [m k] (assoc m k (reduce #(+ %1 (get %2 k 0)) 0 ms)))
+          {} all-keys))
+
+(defn freqs-after-growth**
+  "Returns the frequencies of elements in a chain after a number of reactions.
+
+  Finally figured out how to handle the caching/pre-computation."
+  [chain rules num-times]
+  (let [elements (all-keys rules)
+        add-freqs (partial add-freqs* elements)
+        precomputes (reduce (fn [lookups [x y n]]
+                              (assoc
+                               lookups [x y n]
+                               (if (zero? n)
+                                 (if (= x y) {x 2} {x 1 y 1})
+                                 (if-let [insert (rules [x y])]
+                                   (add-freqs {insert -1}
+                                              (lookups [x insert (dec n)])
+                                              (lookups [insert y (dec n)]))
+                                   (if (= x y) {x 2} {x 1 y 1})))))
+                            {}
+                            (for [n (range (inc num-times))
+                                  [x y] (for [x elements y elements] [x y])]
+                              [x y n]))
+        overcount-correction (->> chain
+                                  (drop 1)
+                                  reverse
+                                  (drop 1)
+                                  (map (fn [x] {x -1}))
+                                  (reduce add-freqs {}))]
+    (->> (zip chain (drop 1 chain) (repeat num-times))
+         (map precomputes)
+         (apply add-freqs)
+         (add-freqs overcount-correction))))
+
+(defn file->big-small-diff* [filename]
+  (let [rules (file->rules filename)
+        chain (file->chain filename)
+        freqs (freqs-after-growth** chain rules 40)]
+    (- (-> freqs vals sort last) (-> freqs vals sort first))))
+
+;;;
+
+(advent/defrunner 1 file->big-small-diff "Difference between biggest and smallest")
+(advent/defrunner 2 file->big-small-diff* "Difference between biggest and smallest*")
---
+++ test/clj/net/bjoc/advent/year_2021/day_14_test.clj
@@ -0,0 +1,47 @@
+(ns net.bjoc.advent.year-2021.day-14-test
+  (:use [clojure.test :only [is testing deftest]]
+        [net.bjoc.advent.year-2021.day-14]))
+
+(deftest grow-by-examples
+  (let [filename "data/year_2021/day_14_example1.input"
+        grow (grow-fn (file->rules filename))
+        chain (file->chain filename)]
+    (is (= "NCNBCHB" (grow chain 1)))
+    (is (= "NBCCNBBBCBHCB" (grow chain 2)))
+    (is (= "NBBBCNCCNBBNBNBBCHBHHBCHB" (grow chain 3)))
+    (is (= "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB" (grow chain 4)))))
+
+(deftest freqs-after-growth-by-examples
+  (let [filename "data/year_2021/day_14_example1.input"
+        chain (file->chain filename)
+        rules (file->rules filename)]
+    (is (= 1749 ((freqs-after-growth chain rules 10) \B)))
+    (is (= 298 ((freqs-after-growth chain rules 10) \C)))
+    (is (= 161 ((freqs-after-growth chain rules 10) \H)))
+    (is (= 865 ((freqs-after-growth chain rules 10) \N)))))
+
+(deftest big-small-diff-by-example
+  (is (= 1588 (file->big-small-diff "data/year_2021/day_14_example1.input"))))
+
+(deftest freqs-after-growth*-by-examples
+  (let [filename "data/year_2021/day_14_example1.input"
+        chain (file->chain filename)
+        rules (file->rules filename)]
+    (is (= 1749 ((freqs-after-growth* chain rules 10) \B)))
+    (is (= 298 ((freqs-after-growth* chain rules 10) \C)))
+    (is (= 161 ((freqs-after-growth* chain rules 10) \H)))
+    (is (= 865 ((freqs-after-growth* chain rules 10) \N)))))
+
+(deftest freqs-after-growth**-by-examples
+  (let [filename "data/year_2021/day_14_example1.input"
+        chain (file->chain filename)
+        rules (file->rules filename)]
+    (is (= 1749 ((freqs-after-growth** chain rules 10) \B)))
+    (is (= 298 ((freqs-after-growth** chain rules 10) \C)))
+    (is (= 161 ((freqs-after-growth** chain rules 10) \H)))
+    (is (= 865 ((freqs-after-growth** chain rules 10) \N)))
+    (is (= 2192039569602 ((freqs-after-growth** chain rules 40) \B)))
+    (is (= 3849876073 ((freqs-after-growth** chain rules 40) \H)))))
+
+(deftest big-small-diff*-by-example
+  (is (= 2188189693529 (file->big-small-diff* "data/year_2021/day_14_example1.input"))))
